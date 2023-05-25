b40434c1b1274bb90f2facb81b1a14bf604e8d5e
Bruce Cantarim <bruce@cantarim.com>
2023-05-25 15:48:14 -0700

    🐛 fix(app): Removed play from Raise and Lower

--- keyboards/preonic/keymaps/workhorse/keymap.c
+++ keyboards/preonic/keymaps/workhorse/keymap.c
@@ -206,7 +206,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      | Home | End  |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |     Play    |      | Prev | Vol- | Vol+ | Next |
+     * |      |      |      |QWERTY|      |     Play    |      | Prev | Vol- | Vol+ | Next |
      * `-----------------------------------------------------------------------------------'
      */
     [_LOWER] = LAYOUT_preonic_grid(
@@ -214,7 +214,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_PIPE,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F6,   KC_ENT,
     KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_HOME, KC_END,  _______, KC_RSFT,
-    _______, _______, _______, QWERTY,  _______, KC_MPLY, KC_MPLY, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+    _______, _______, _______, QWERTY,  _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
     ),
 
     /* Lower with inverted T arrows
@@ -227,7 +227,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      | Home | End  | Vol+ |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |     Play    |      |      | Prev | Vol- | Next |
+     * |      |      |      |QWERTY|      |     Play    |      |      | Prev | Vol- | Next |
      * `-----------------------------------------------------------------------------------'
      */
     [_LOWER_TARROWS] = LAYOUT_preonic_grid(
@@ -235,7 +235,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_PIPE,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F6,   KC_ENT,
     KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_HOME, KC_END,  KC_VOLU, KC_RSFT,
-    _______, _______, _______, QWERTY,  _______, KC_MPLY, KC_MPLY, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
+    _______, _______, _______, QWERTY,  _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
     ),
 
 
@@ -249,7 +249,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      | Pg Up| Pg Dn|      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |     Play    |      | Prev | Vol- | Vol+ | Next |
+     * |      |      |      |QWERTY|      |     Play    |      | Prev | Vol- | Vol+ | Next |
      * `-----------------------------------------------------------------------------------'
      */
     [_RAISE] = LAYOUT_preonic_grid(
@@ -257,7 +257,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
     KC_CAPS, KC_F1,   KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F6,   KC_ENT,
     _______, KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  _______, KC_PGUP, KC_PGDN, _______, KC_RSFT,
-    _______, _______, _______, QWERTY, _______, KC_MPLY, KC_MPLY, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+    _______, _______, _______, QWERTY, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
     ),
 
     /* Raise with inverted T arrows
@@ -270,7 +270,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      | Pg Up| Pg Dn| Vol+ |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |     Play    |      |      | Prev | Vol+ | Next |
+     * |      |      |      |QWERTY|      |     Play    |      |      | Prev | Vol+ | Next |
      * `-----------------------------------------------------------------------------------'
      */
     [_RAISE_TARROWS] = LAYOUT_preonic_grid(
@@ -278,7 +278,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
     KC_CAPS, KC_F1,   KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F6,   KC_ENT,
     _______, KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  _______, KC_PGUP, KC_PGDN, KC_VOLU, KC_RSFT,
-    _______, _______, _______, QWERTY, _______, KC_MPLY, KC_MPLY, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
+    _______, _______, _______, QWERTY, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
     ),
 
     /* Adjust (Lower + Raise)
@@ -376,9 +376,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 layer_on(_LOWER);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
+                rgblight_set_layer_state(1, true);
             } else {
                 layer_off(_LOWER);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
+                rgblight_set_layer_state(1, false);
             }
             return false;
             break;
@@ -386,9 +388,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 layer_on(_RAISE);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
+                rgblight_set_layer_state(2, true);
             } else {
                 layer_off(_RAISE);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
+                rgblight_set_layer_state(2, false);
             }
             return false;
             break;
